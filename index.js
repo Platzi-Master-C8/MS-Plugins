34ac37052ff8b0a63a62e045a18bca609ce953c4
César Velandia <cesarmvelandiaf@gmail.com>
2022-02-02 16:19:15 -0500

    Added env to Host

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const origins = [
 // Server definition
 const server = Hapi.server({
   port: process.env.PORT || 3000,
-  host: 'localhost',
+  host: process.env.HOSTNAME || 'localhost',
   routes: {
     cors: {
       origin: origins,
